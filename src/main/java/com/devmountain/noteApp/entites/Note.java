14ef6eee00fe367cc287a337adaf26014218766f
ckyle121 <carolinekyle21@gmail.com>
2022-11-29 19:27:54 -0600

    use Lombok for getters/setters/contructors

--- src/main/java/com/devmountain/noteApp/entites/Note.java
+++ src/main/java/com/devmountain/noteApp/entites/Note.java
@@ -1,9 +1,16 @@
 package com.devmountain.noteApp.entites;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "Notes")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Note {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -12,28 +19,4 @@ public class Note {
     @Column(columnDefinition = "text")
     private String body;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getBody() {
-        return body;
-    }
-
-    public void setBody(String body) {
-        this.body = body;
-    }
-
-    public Note(){
-
-    }
-
-    public Note(Long id, String body) {
-        this.id = id;
-        this.body = body;
-    }
 }
